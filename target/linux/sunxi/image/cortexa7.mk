f1c319a14f467ef8812c9243f8bf2e18d0f040e6
Robert <robert@axium.co.nz>
2022-04-05 06:16:23 +1200

    Removed contexa7v3sspinand sub target. Now v3sspinand variants will be devices in the cortexa7 target Added ax255-media-manager device

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -232,11 +232,79 @@ define Device/xunlong_orangepi-2
 endef
 TARGET_DEVICES += xunlong_orangepi-2
 
-define Device/licheepi-zero
-  DEVICE_VENDOR := LicheePi
-  DEVICE_MODEL := Zero
-  DEVICE_PACKAGES:=kmod-rtl8723bs rtl8723bs-firmware
-  SOC := sun8i-v3s
-  SUPPORTED_DEVICES:=licheepi,licheepi-zero
-endef
-TARGET_DEVICES += licheepi-zero
+ifeq ($(CONFIG_SUN8I_V3S_SPINAND_OPTIONS),y)
+  define Device/licheepi-zero-spinand
+    DEVICE_VENDOR := LicheePi
+    DEVICE_MODEL := Zero
+    DEVICE_PACKAGES:=kmod-rtl8723bs rtl8723bs-firmware
+    SOC := sun8i-v3s
+    SUPPORTED_DEVICES:=licheepi,licheepi-zero
+
+    KERNEL := kernel-bin | lzma | uImage lzma | sunxi-kernelubifs
+    IMAGES := ubispinand.img.gz
+
+    MKUBIFS_OPTS := -F -m $(CONFIG_SUNXI_SPINAND_PAGESIZE) -e $(shell echo $$(($(CONFIG_SUNXI_SPINAND_BLOCKSIZE) - (($(CONFIG_SUNXI_SPINAND_PAGESIZE)/1024)*2))))KiB -c 880 -U
+    UBINIZE_OPTS := -vv
+
+    BLOCKSIZE := $(CONFIG_SUNXI_SPINAND_BLOCKSIZE)KiB
+    PAGESIZE := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
+    SUBPAGESIZE := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
+    VID_HDR_OFFSET := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
+    IMAGE_SIZE := $(CONFIG_TARGET_ROOTFS_PARTSIZE)m
+    KERNEL_IN_UBI := 1
+    UBOOTENV_IN_UBI := 1
+    # UBINIZE_PARTS := dtb=$(DTS_DIR)/$$(SUNXI_DTS).dtb=1
+    IMAGE/ubispinand.img.gz := \
+        sunxi-spinandboot | \
+        pad-to $$(CONFIG_SUN8I_V3S_OFFSET_UBI) | \
+        append-ubi | \
+        gzip
+  endef
+  TARGET_DEVICES += licheepi-zero-spinand
+  
+  define Device/ax255-media-manager
+    DEVICE_VENDOR := Axium
+    DEVICE_MODEL := AX255 MediaManager
+    DEVICE_PACKAGES:=kmod-rtl8723bs rtl8723bs-firmware
+    SOC := sun8i-v3s
+    SUPPORTED_DEVICES:=licheepi,licheepi-zero
+
+    KERNEL := kernel-bin | lzma | uImage lzma | sunxi-kernelubifs
+    IMAGES := ubispinand.img.gz
+
+    MKUBIFS_OPTS := -F -m $(CONFIG_SUNXI_SPINAND_PAGESIZE) -e $(shell echo $$(($(CONFIG_SUNXI_SPINAND_BLOCKSIZE) - (($(CONFIG_SUNXI_SPINAND_PAGESIZE)/1024)*2))))KiB -c 880 -U
+    UBINIZE_OPTS := -vv
+
+    BLOCKSIZE := $(CONFIG_SUNXI_SPINAND_BLOCKSIZE)KiB
+    PAGESIZE := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
+    SUBPAGESIZE := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
+    VID_HDR_OFFSET := $(CONFIG_SUNXI_SPINAND_PAGESIZE)
+    IMAGE_SIZE := $(CONFIG_TARGET_ROOTFS_PARTSIZE)m
+    KERNEL_IN_UBI := 1
+    UBOOTENV_IN_UBI := 1
+    # UBINIZE_PARTS := dtb=$(DTS_DIR)/$$(SUNXI_DTS).dtb=1
+    IMAGE/ubispinand.img.gz := \
+        sunxi-spinandboot | \
+        pad-to $$(CONFIG_SUN8I_V3S_OFFSET_UBI) | \
+        append-ubi | \
+        gzip
+  endef
+else
+  define Device/licheepi-zero
+    DEVICE_VENDOR := LicheePi
+    DEVICE_MODEL := Zero
+    DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-rtl8723bs rtl8723bs-firmware
+    SOC := sun8i-v3s
+    SUPPORTED_DEVICES:=licheepi,licheepi-zero
+  endef
+  TARGET_DEVICES += licheepi-zero
+  
+  define Device/ax255-media-manager
+    DEVICE_VENDOR := Axium
+    DEVICE_MODEL := AX255 MediaManager
+    DEVICE_PACKAGES:=kmod-rtl8723bs rtl8723bs-firmware
+    SOC := sun8i-v3s
+    SUPPORTED_DEVICES:=licheepi,licheepi-zero
+  endef
+endif
+TARGET_DEVICES += ax255-media-manager
--- target/linux/sunxi/image/gen_sunxi_kernel_ubifs.sh
+++ target/linux/sunxi/image/gen_sunxi_kernel_ubifs.sh
@@ -28,20 +28,16 @@ LEBS=$((($TOTALSIZE / ($LEBSIZE*1024)) + 17))
 
 WORKDIR=$(mktemp -d)
 
-# cp "$KERNEL" "$WORKDIR/uImage"
-# cp "$DTB" "$WORKDIR/dtb"
-## if wanting to preserve the input files
-# cp "$KERNEL" /home/robert/uImage
-# cp "$DTB" /home/robert/dtb
-
-# Special axium hack - load 5.10 kernel as the ubi kernel section by hardcoding paths to the image and dtb.
-cp /home/robert/Documents/openwrt/21.02-CUSTOM_DIFF/5.10_image/boot/spinand/uImage "$WORKDIR/uImage"
-cp /home/robert/Documents/openwrt/21.02-CUSTOM_DIFF/5.10_image/boot/spinand/dtb "$WORKDIR/dtb"
+cp "$KERNEL" "$WORKDIR/uImage"
+cp "$DTB" "$WORKDIR/dtb"
+## preserve the input files
+cp "$KERNEL" ~/uImage
+cp "$DTB" ~/dtb
 
 # MKUBIFS=mkfs.ubifs
-MKUBIFS=/home/robert/Documents/openwrt/openwrt-master/staging_dir/host/bin/mkfs.ubifs
+MKUBIFS=$STAGING_DIR_HOST/bin/mkfs.ubifs
 
-# printf "%s -o %s -F -m %u -e %uKiB -c %u -U -v -r <WORKDIR>\n" "$MKUBIFS" "$KERNEL.new" $PAGESIZE $LEBSIZE $LEBS >> /home/robert/image.out
+# printf "%s -o %s -F -m %u -e %uKiB -c %u -U -v -r <WORKDIR>\n" "$MKUBIFS" "$KERNEL.new" $PAGESIZE $LEBSIZE $LEBS >> ~/image.out
 
 $MKUBIFS -o "$KERNEL.new" -F -m $PAGESIZE -e ${LEBSIZE}KiB -c $LEBS -U -v -r $WORKDIR
 
